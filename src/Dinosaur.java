51d7c5cf1098aa54e3dbbe11171317b634d540fc
afruzbekh <afruzbek1595@gmail.com>
2024-08-01 17:41:38 +0500

    5:41

---
+++ src/Dinosaur.java
@@ -0,0 +1,26 @@
+// public class secondTest {
+//     public static void main(String[] args) {
+//         System.out.println("what is the texxxttt");
+//     }
+// }
+
+// public class thirdTest {
+//     public static void main(String[] args) {
+//         System.out.println("Ada Lovelace");
+//     }
+
+// }
+
+// public class OnceUponATime {
+//     public static void main(String[] args) {
+//        System.out.println();
+//     }
+// }
+
+public class Dinosaur {
+    public static void main(String[] args) {
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+    }
+}
