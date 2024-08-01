628c6f527079ef5925513ee6af0f8d3d2e93e643
afruzbekh <afruzbek1595@gmail.com>
2024-08-01 16:11:30 +0500

    Java roadmap beginning...

---
+++ src/thirdTest.java
@@ -0,0 +1,12 @@
+// public class secondTest {
+//     public static void main(String[] args) {
+//         System.out.println("what is the texxxttt");
+//     }
+// }
+
+public class thirdTest {
+    public static void main(String[] args) {
+
+    }
+
+}
