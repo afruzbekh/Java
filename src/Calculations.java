6a27b8c029fa608a675b21c828389e36d591c423
afruzbekh <afruzbek1595@gmail.com>
2024-09-06 14:09:16 +0500

    nothing

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,65 +1,65 @@
 // public class secondTest {
-//     public static void main(String[] args) {
-//         System.out.println("what is the texxxttt");
-//     }
+// public static void main(String[] args) {
+// System.out.println("what is the texxxttt");
+// }
 // }
 
 // public class thirdTest {
-//     public static void main(String[] args) {
-//         System.out.println("Ada Lovelace");
-//     }
+// public static void main(String[] args) {
+// System.out.println("Ada Lovelace");
+// }
 // }
 // public class OnceUponATime {
-//     public static void main(String[] args) {
-//        System.out.println();
-//     }
+// public static void main(String[] args) {
+// System.out.println();
+// }
 // }
 // public class Dinosaur {
-//     public static void main(String[] args) {
-//         System.out.println("Once upon a time");
-//         System.out.println("there was");
-//         System.out.println("a dinosaur");
-//     }
+// public static void main(String[] args) {
+// System.out.println("Once upon a time");
+// System.out.println("there was");
+// System.out.println("a dinosaur");
+// }
 // }
 // import java.util.Scanner;
 // public class Prompt {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.println("Write a message:");
-//         String message = scanner.nextLine();
-//         System.out.println(message);
-//     }
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
+// System.out.println("Write a message:");
+// String message = scanner.nextLine();
+// System.out.println(message);
+// }
 // }
 // import java.util.Scanner;
 // public class PromptTaskOne {
-//     public static void main(String[] args) {
-//         Scanner notScanner = new Scanner(System.in);
-//         System.out.println("Write a message:");     
-//         String promptMessage = notScanner.nextLine();
-//         System.out.println(promptMessage);
-//         notScanner.close();
-//         String number = "higger";
-//         System.out.println(number + "... wassssssssssssssssuuuuuuuuuuuuuuuuuuuup");
-//         String start = "My name is ";
-//         String end = "James Bond!";
-//         System.out.println(start + "Bond, " + end);
-//     }
+// public static void main(String[] args) {
+// Scanner notScanner = new Scanner(System.in);
+// System.out.println("Write a message:");
+// String promptMessage = notScanner.nextLine();
+// System.out.println(promptMessage);
+// notScanner.close();
+// String number = "higger";
+// System.out.println(number + "... wassssssssssssssssuuuuuuuuuuuuuuuuuuuup");
+// String start = "My name is ";
+// String end = "James Bond!";
+// System.out.println(start + "Bond, " + end);
+// }
 // }
 // import java.util.Scanner;
 // public class Program {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.println("Write something: ");
-//         String message = scanner.nextLine();
-//         System.out.println("You wrote " + message);
-//     }
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
+// System.out.println("Write something: ");
+// String message = scanner.nextLine();
+// System.out.println("You wrote " + message);
+// }
 // }
 // import java.util.Scanner;
 // public class Greeting {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
 // Scanner reader = new Scanner(System.in);
-// System.out.println("--------  What is your name? --------");
+// System.out.println("-------- What is your name? --------");
 // String name = scanner.nextLine();
 // System.out.println("Good evening Mr." + name);
 // System.out.println("Write the first string:");
@@ -86,19 +86,24 @@
 // System.out.println("was preceded by " + second+ ".");
 // System.out.println("The first string was " + first + ".");
 // System.out.println("All together: " + first + second + third);
-// System.out.println("-------- I will tell you a story, but I need some information first, Okay? --------");
+// System.out.println("-------- I will tell you a story, but I need some
+// information first, Okay? --------");
 // String permission = scanner.nextLine();
-// System.out.println("-------- Great! Now, What is the main character called? ---------");
+// System.out.println("-------- Great! Now, What is the main character called?
+// ---------");
 // String characterName = scanner.nextLine();
 // System.out.println("-------- What is their job?---------");
 // String characterJob = scanner.nextLine();
 // System.out.println("-------- Here is the story:--------");
-// System.out.println("Once upon a time there was " + characterName + ", who was a " + characterJob + ". On the way to work, " + characterName + " reflected on life. Perhaps " + characterName + " will not be a " + characterJob + " forever.");
+// System.out.println("Once upon a time there was " + characterName + ", who was
+// a " + characterJob + ". On the way to work, " + characterName + " reflected
+// on life. Perhaps " + characterName + " will not be a " + characterJob + "
+// forever.");
 // }
 // }
 // import java.util.Scanner;
 // public class DataTypes {
-//     public static void main(String[] args) {
+// public static void main(String[] args) {
 // String text = "contains text";
 // int wholeNumber = 123;
 // double floatingPoint = 3.141592653;
@@ -130,7 +135,7 @@
 // double surfaceArea = pi * radius * radius;
 // System.out.println(surfaceArea);
 // Scanner scanner = new Scanner(System.in);
-// System.out.println("Give a number:  ");
+// System.out.println("Give a number: ");
 // String text = scanner.nextLine();
 // System.out.println("You gave the number " + text);
 // Scanner scanner = new Scanner(System.in);
@@ -161,72 +166,76 @@
 // scanner.close();
 // }
 // }
-import java.util.Scanner;
 
-public class Calculations {
+// import java.util.Scanner;
 
-    public static void main(String[] args) {
+// public class Calculations {
 
-        Scanner scanner = new Scanner(System.in);
+// public static void main(String[] args) {
 
-        // int num1 = 122;
-        // System.out.println(num1);
-        // int num2 = 122;
-        // System.out.println(num2);
-        // System.out.println(num1 + num2);
-        // System.out.println("------------- How many days would you like to convert to seconds? ----------------");
-        // int number = Integer.valueOf(scanner.nextLine());
-        // System.out.println(number + " days are equal to " + number * 86400 + " seconds!");
-        // int first = 10;
-        // int second = first + 5;
-        // first = 20;
-        // System.out.println(second);        
-        // int age = 32;
-        // int height = 178;
-        // age = height + 10;
-        // System.out.println(age);
-        // System.out.println("Four: " + (2 + 2));
-        // System.out.println("But! Twenty-two: " + 2 + 2);
-        // System.out.println("Give the first number: ");
-        // int first = Integer.valueOf(scanner.nextLine());
-        // System.out.println("Give the second number: ");
-        // int second = Integer.valueOf(scanner.nextLine());
-        // System.out.println("Give the third number: ");
-        // int third = Integer.valueOf(scanner.nextLine());
-        // int sum = first + second + third;
-        // System.out.println(first + " + " + second + " + " + third + " = " + sum);
-        // scanner.close();
-        // System.out.println("Give the first number:");
-        // int num1 = Integer.valueOf(scanner.nextLine());
-        // System.out.println("Give the second number:");
-        // int num2 = Integer.valueOf(scanner.nextLine());
-        // System.out.println("Give the third number:");
-        // int num3 = Integer.valueOf(scanner.nextLine());
-        // double average = (num1 + num2 + num3) / 2.0;
-        // System.out.println("The average is " + average);
-        // int dividend = 3;
-        // int divisor = 2;
-        // double result = (double) dividend / divisor * 1.0;
-        // System.out.println(result);
-        // System.out.println("Give the first number: ");
-        // int firstNumber = Integer.valueOf(scanner.nextLine());
-        // System.out.println("Give the second number: ");
-        // int secondNumber = Integer.valueOf(scanner.nextLine());
-        // int addition = firstNumber + secondNumber;
-        // int subtraction = firstNumber - secondNumber;
-        // int multiplication = firstNumber * secondNumber;
-        // int division = firstNumber / secondNumber;
-        // System.out.println(firstNumber + " + " + secondNumber + " = " + addition);
-        // System.out.println(firstNumber + " - " + secondNumber + " = " + subtraction);
-        // System.out.println(firstNumber + " * " + secondNumber + " = " + multiplication);
-        // System.out.println(firstNumber + " / " + secondNumber + " = " + division);
-        // scanner.close();
+// Scanner scanner = new Scanner(System.in);
 
-        // int second = 32;
-        // int first = 25;
-        // second = 17;
-        // first = second;
+// int num1 = 122;
+// System.out.println(num1);
+// int num2 = 122;
+// System.out.println(num2);
+// System.out.println(num1 + num2);
+// System.out.println("------------- How many days would you like to convert to
+// seconds? ----------------");
+// int number = Integer.valueOf(scanner.nextLine());
+// System.out.println(number + " days are equal to " + number * 86400 + "
+// seconds!");
+// int first = 10;
+// int second = first + 5;
+// first = 20;
+// System.out.println(second);
+// int age = 32;
+// int height = 178;
+// age = height + 10;
+// System.out.println(age);
+// System.out.println("Four: " + (2 + 2));
+// System.out.println("But! Twenty-two: " + 2 + 2);
+// System.out.println("Give the first number: ");
+// int first = Integer.valueOf(scanner.nextLine());
+// System.out.println("Give the second number: ");
+// int second = Integer.valueOf(scanner.nextLine());
+// System.out.println("Give the third number: ");
+// int third = Integer.valueOf(scanner.nextLine());
+// int sum = first + second + third;
+// System.out.println(first + " + " + second + " + " + third + " = " + sum);
+// scanner.close();
+// System.out.println("Give the first number:");
+// int num1 = Integer.valueOf(scanner.nextLine());
+// System.out.println("Give the second number:");
+// int num2 = Integer.valueOf(scanner.nextLine());
+// System.out.println("Give the third number:");
+// int num3 = Integer.valueOf(scanner.nextLine());
+// double average = (num1 + num2 + num3) / 2.0;
+// System.out.println("The average is " + average);
+// int dividend = 3;
+// int divisor = 2;
+// double result = (double) dividend / divisor * 1.0;
+// System.out.println(result);
+// System.out.println("Give the first number: ");
+// int firstNumber = Integer.valueOf(scanner.nextLine());
+// System.out.println("Give the second number: ");
+// int secondNumber = Integer.valueOf(scanner.nextLine());
+// int addition = firstNumber + secondNumber;
+// int subtraction = firstNumber - secondNumber;
+// int multiplication = firstNumber * secondNumber;
+// int division = firstNumber / secondNumber;
+// System.out.println(firstNumber + " + " + secondNumber + " = " + addition);
+// System.out.println(firstNumber + " - " + secondNumber + " = " + subtraction);
+// System.out.println(firstNumber + " * " + secondNumber + " = " +
+// multiplication);
+// System.out.println(firstNumber + " / " + secondNumber + " = " + division);
+// scanner.close();
+// int second = 32;
+// int first = 25;
+// second = 17;
+// first = second;
+// System.out.println(second);
+// }
+// }
 
-        // System.out.println(second);
-    }
-}
+
