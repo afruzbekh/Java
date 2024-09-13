894af97a7f69bff5a7505ca66571297ec001178e
afruzbekh <afruzbek1595@gmail.com>
2024-09-13 17:36:02 +0500

    loop part #1

--- src/loop.java
+++ src/loop.java
@@ -236,164 +236,323 @@
 // }
 // }
 
+// import java.util.Scanner;
+
+// public class Conditional_Statements {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         Scanner reader = new Scanner(System.in);
+
+// System.out.println("wassup niggas");
+// if (true) {
+// System.out.println("This code is unavoidable!");
+// }
+
+// int number = 11;
+// if (number > 10) {
+// System.out.println("The number was greater than 10");
+// }
+
+// System.out.println("Give speed: ");
+// int speedingTicketChecker = Integer.valueOf(scanner.nextLine());
+// if (speedingTicketChecker >= 120) {
+// System.out.println("Speeding ticket!");
+// }
+
+// int number = 5500;
+
+// if (number != 0) {
+// System.out.println("The number is not equal to 0");
+// }
+
+// if (number >= 1000) {
+// System.out.println("The number is at least 1000");
+// }
+
+// System.out.println("Give a number: ");
+// int exactYear = Integer.valueOf(scanner.nextLine());
+// if (exactYear <= 2015) {
+// System.out.println("Ancient history!");
+// }
+
+// int number = 6;
+
+// if (number > 5) {
+// System.out.println("Your number is greater than five!");
+// }else {
+// System.out.println("Your number is five or less!");
+// }
+
+// System.out.println("How old are you?");
+// int PositiveNegative = Integer.valueOf(scanner.nextLine());
+// if (PositiveNegative >= 18) {
+// System.out.println("You are an adult");
+// }else {
+// System.out.println("You are not an adult");
+// }
+
+// int number = 3;
+
+// if (number == 1) {
+// System.out.println("The number is one");
+// } else if (number == 2) {
+// System.out.println("The given number is two");
+// } else if (number == 3) {
+// System.out.println("The number must be three!");
+// } else {
+// System.out.println("Something else!");
+// }
+
+// System.out.println("Give the first number:");
+// int firstNumber = Integer.valueOf(scanner.nextLine());
+
+// System.out.println("Give the second number:");
+// int secondNumber = Integer.valueOf(scanner.nextLine());
+
+// if(firstNumber > secondNumber) {
+// System.out.println("Greater number is: " + firstNumber);
+// }else if (secondNumber > firstNumber) {
+// System.out.println("Greater number is: " + secondNumber);
+// }else {
+// System.out.println("The numbers are equal!");
+// }
+
+// System.out.print("Enter the total points earned: ");
+// int points = scanner.nextInt();
+
+// String grade;
+
+// if (points < 0) {
+// grade = "impossible!";
+// } else if (points < 50) {
+// grade = "failed";
+// } else if (points < 60) {
+// grade = "1";
+// } else if (points < 70) {
+// grade = "2";
+// } else if (points < 80) {
+// grade = "3";
+// } else if (points < 90) {
+// grade = "4";
+// } else if (points <= 100) {
+// grade = "5";
+// } else {
+// grade = "incredible!";
+// }
+
+// System.out.println("Your grade is: " + grade);
+
+// System.out.print("Give a number: ");
+// int number = scanner.nextInt();
+
+// if (number % 2 == 0) {
+// System.out.println("Number " + number + " is even.");
+// } else {
+// System.out.println("Number " + number + " is odd.");}
+
+// System.out.println("Enter a string");
+// String input = reader.nextLine();
+
+// if (input.equals("a string")) {
+// System.out.println("Great! You read the instructions correctly.");
+// } else {
+// System.out.println("Missed the mark!");
+// }
+
+// System.out.println("Password?");
+// String passwordInput = reader.nextLine();
+
+// if (passwordInput.equals("Caput Draconis")) {
+// System.out.println("Welcome back, Boss!");
+// }else {
+// System.out.println("Get the fuck out of here!!");
+// }
+
+// System.out.println("Is the number within the range 5-10: ");
+// int number = 7;
+
+// if (number >= 5 && number <= 10) {
+// System.out.println("It is! :)");
+// } else {
+// System.out.println("It is not :(");
+// }
+
+// System.out.println("How old are you?");
+// int agePrompt = Integer.valueOf(scanner.nextLine());
+
+// if (agePrompt >= 0 && agePrompt <= 120) {
+// System.out.println("OK!");
+// } else {
+// System.out.println("Impossible!");
+// }
+
+// System.out.println("Give a year:");
+// int yearPrompt = Integer.valueOf(scanner.nextLine());
+
+// if (yearPrompt % 4 == 0 && (yearPrompt % 100 != 0 || yearPrompt % 400 == 0))
+// {
+// System.out.println("The year is a LEAP year.");
+// } else {
+// System.out.println("The year is not a leap year.");
+// }
+
+//         System.out.println("-------- Value of the gift? ---------");
+//         double giftValue = scanner.nextDouble();
+//         double tax;
+
+//         if (giftValue < 5000) {
+//             System.out.println("No tax!");
+//         } else if (giftValue <= 25000) {
+//             tax = 100 + (giftValue - 5000) * 0.08;
+//             System.out.println("Tax: " + tax);
+//         } else if (giftValue <= 55000) {
+//             tax = 1700 + (giftValue - 25000) * 0.10;
+//             System.out.println("Tax: " + tax);
+//         } else if (giftValue <= 200000) {
+//             tax = 4700 + (giftValue - 55000) * 0.12;
+//             System.out.println("Tax: " + tax);
+//         } else if (giftValue <= 1000000) {
+//             tax = 22100 + (giftValue - 200000) * 0.15;
+//             System.out.println("Tax: " + tax);
+//         } else {
+//             tax = 142100 + (giftValue - 1000000) * 0.17;
+//             System.out.println("Tax: " + tax);
+//         }
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class part2 {
+//     public static void main(String[] main) {
+//         Scanner scanner = new Scanner(System.in);
+
+// int number = Integer.valueOf(scanner.nextLine());
+// int result = number * 2;
+// System.out.println(result);
+
+// int squareNumber = Integer.valueOf(scanner.nextLine());
+// int squareNumber2 = Integer.valueOf(scanner.nextLine());
+// System.out.println(Math.sqrt(squareNumber + squareNumber2));
+
+// int number = 12;
+
+// if (number > 10) {
+//     System.out.print("Hello ");
+// } else if (number < 20) {
+//     System.out.print("world");
+// } else {
+//     System.out.print("!");
+// }
+
+// System.out.println("---------- Give a number: ---------");
+// int numPrompt = Integer.valueOf(scanner.nextLine());
+// if (numPrompt < 0) {
+//     System.out.println(numPrompt * (-1));
+// } else {
+//     System.out.println(numPrompt);
+// }
+
+//     }
+// }
+
 import java.util.Scanner;
 
-public class Conditional_Statements {
+public class loop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Scanner reader = new Scanner(System.in);
 
-        // System.out.println("wassup niggas");
-        // if (true) {
-        // System.out.println("This code is unavoidable!");
+        // int sum = 0;
+
+        // System.out.println("Input a number: ");
+        // sum = sum + Integer.valueOf(scanner.nextLine());
+
+        // System.out.println("Input a number: ");
+        // sum = sum + Integer.valueOf(scanner.nextLine());
+
+        // System.out.println("Input a number: ");
+        // sum = sum + Integer.valueOf(scanner.nextLine());
+
+        // System.out.println("Input a number: ");
+        // sum = sum + Integer.valueOf(scanner.nextLine());
+
+        // System.out.println("Input a number: ");
+        // sum = sum + Integer.valueOf(scanner.nextLine());
+
+        // System.out.println("The sum of the numbers is " + sum);
+
+        // int numbersRead = 0;
+        // int sum = 0;
+
+        // while (true) {
+        // if (numbersRead == 5) {
+        // break;
         // }
 
-        // int number = 11;
-        // if (number > 10) {
-        // System.out.println("The number was greater than 10");
+        // System.out.println("Input number");
+        // sum = sum + Integer.valueOf(scanner.nextLine());
+        // numbersRead = numbersRead + 1;
         // }
 
-        // System.out.println("Give speed: ");
-        // int speedingTicketChecker = Integer.valueOf(scanner.nextLine());
-        // if (speedingTicketChecker >= 120) {
-        // System.out.println("Speeding ticket!");
+        // System.out.println("The sum of the numbers is " + sum);
+
+        // while (true) {
+        // System.out.println("I can program!");
         // }
 
-        // int number = 5500;
+        // int number = 1;
 
-        // if (number != 0) {
-        // System.out.println("The number is not equal to 0");
+        // while (true) {
+        // System.out.println(number);
+        // if (number >= 5) {
+        // break;
         // }
 
-        // if (number >= 1000) {
-        // System.out.println("The number is at least 1000");
+        // number = number + 1;
         // }
 
-        // System.out.println("Give a number: ");
-        // int exactYear = Integer.valueOf(scanner.nextLine());
-        // if (exactYear <= 2015) {
-        // System.out.println("Ancient history!");
+        // System.out.println("Ready!");
+
+        // while (true) {
+        // System.out.println("Exit? (y exits)");
+        // String input = scanner.nextLine();
+        // if (input.equals("y")) {
+        // break;
         // }
 
-        // int number = 6;
-
-        // if (number > 5) {
-        // System.out.println("Your number is greater than five!");
-        // }else {
-        // System.out.println("Your number is five or less!");
+        // System.out.println("Ok! Let's carry on!");
         // }
 
-        // System.out.println("How old are you?");
-        // int PositiveNegative = Integer.valueOf(scanner.nextLine());
-        // if (PositiveNegative >= 18) {
-        // System.out.println("You are an adult");
-        // }else {
-        // System.out.println("You are not an adult");
+        // System.out.println("Ready!");
+
+        // while (true) {
+        // System.out.println("---------- Shall we carry on? ---------");
+        // String answerToStop = scanner.nextLine();
+
+        // if (answerToStop.equals("no")) {
+        // break;
+        // }
         // }
 
-        // int number = 3;
+        // while (true) {
+        //     System.out.println("Input a number, 0 to quit");
+        //     int command = Integer.valueOf(scanner.nextLine());
+        //     if (command == 0) {
+        //         break;
+        //     }
 
-        // if (number == 1) {
-        // System.out.println("The number is one");
-        // } else if (number == 2) {
-        // System.out.println("The given number is two");
-        // } else if (number == 3) {
-        // System.out.println("The number must be three!");
-        // } else {
-        // System.out.println("Something else!");
+        //     System.out.println("You input " + command);
         // }
 
-        // System.out.println("Give the first number:");
-        // int firstNumber = Integer.valueOf(scanner.nextLine());
+        // System.out.println("Done, thank you!");
 
-        // System.out.println("Give the second number:");
-        // int secondNumber = Integer.valueOf(scanner.nextLine());
-
-        // if(firstNumber > secondNumber) {
-        // System.out.println("Greater number is: " + firstNumber);
-        // }else if (secondNumber > firstNumber) {
-        // System.out.println("Greater number is: " + secondNumber);
-        // }else {
-        // System.out.println("The numbers are equal!");
-        // }
-
-        // System.out.print("Enter the total points earned: ");
-        // int points = scanner.nextInt();
-
-        // String grade;
-
-        // if (points < 0) {
-        // grade = "impossible!";
-        // } else if (points < 50) {
-        // grade = "failed";
-        // } else if (points < 60) {
-        // grade = "1";
-        // } else if (points < 70) {
-        // grade = "2";
-        // } else if (points < 80) {
-        // grade = "3";
-        // } else if (points < 90) {
-        // grade = "4";
-        // } else if (points <= 100) {
-        // grade = "5";
-        // } else {
-        // grade = "incredible!";
-        // }
-
-        // System.out.println("Your grade is: " + grade);
-
-        // System.out.print("Give a number: ");
-        // int number = scanner.nextInt();
-
-        // if (number % 2 == 0) {
-        // System.out.println("Number " + number + " is even.");
-        // } else {
-        // System.out.println("Number " + number + " is odd.");}
-
-        // System.out.println("Enter a string");
-        // String input = reader.nextLine();
-
-        // if (input.equals("a string")) {
-        // System.out.println("Great! You read the instructions correctly.");
-        // } else {
-        // System.out.println("Missed the mark!");
-        // }
-
-        // System.out.println("Password?");
-        // String passwordInput = reader.nextLine();
-
-        // if (passwordInput.equals("Caput Draconis")) {
-        // System.out.println("Welcome back, Boss!");
-        // }else {
-        // System.out.println("Get the fuck out of here!!");
-        // }
-
-        // System.out.println("Is the number within the range 5-10: ");
-        // int number = 7;
-
-        // if (number >= 5 && number <= 10) {
-        //     System.out.println("It is! :)");
-        // } else {
-        //     System.out.println("It is not :(");
-        // }
-
-        // System.out.println("How old are you?");
-        // int agePrompt = Integer.valueOf(scanner.nextLine());
-
-        // if (agePrompt >= 0 && agePrompt <= 120) {
-        //     System.out.println("OK!");
-        // } else {
-        //     System.out.println("Impossible!");
-        // }
-
-        System.out.println("Give a year:");
-        int yearPrompt = Integer.valueOf(scanner.nextLine());
-
-        if (yearPrompt % 4 == 0 && (yearPrompt % 100 != 0 || yearPrompt % 400 == 0)) {
-            System.out.println("The year is a LEAP year.");
-        } else {
-            System.out.println("The year is not a leap year.");
+        while (true) {
+            System.out.println("Give a number");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 4) {
+                break;
+            }
         }
-        
     }
 }
